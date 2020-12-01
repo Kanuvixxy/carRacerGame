a862894d4a2508b66cdd4ba09bc4579563295f5b
Vihaan Kharia <vihaankharia8@gmail.com>
2020-12-01 17:52:58 +0530

    Update Game.js

--- js/Game.js
+++ js/Game.js
@@ -27,31 +27,74 @@ class Game
 
             form = new Form();
             form.display();
+
+            
+        car1 = createSprite(100, 200);
+        car2 = createSprite(300, 200);
+        car3 = createSprite(500, 200);
+        car4 = createSprite(700, 200);
+
+        cars = [car1, car2, car3, car4];
+
+        car1.visible = false;
+        car2.visible = false;
+        car3.visible = false;
+        car4.visible = false;
         }
     }
 
     play(){
         form.hide();
-        this.greeting.hide();
 
-        textSize(30);
+        car1.visible = true;
+        car2.visible = true;
+        car3.visible = true;
+        car4.visible = true;
+
+        /*textSize(30);
         text("Game started", 120, 100);
+        */
 
         Player.getPlayerInfo();
 
         if(allPlayers !== undefined){
-            var displayPosition = 130;
+            var index = 0;
+            var x = 0;
+            var y;
+
+            //var displayPosition = 130;
 
             for(var plr in allPlayers){
-            displayPosition += 20;
+                index += 1;
 
-            textSize(15);
-            text(allPlayers[plr].name + ": " + allPlayers[plr].distance, 120, displayPosition);
+                /*if(plr === "player" + player.index){
+                    fill("red")
+                }
+
+                else{
+                    fill("black");
+                }*/
+                x += 200;
+                y = displayHeight - allPlayers[plr].distance;
+
+                cars[index - 1].x = x;
+                cars[index - 1].y = y;
+
+                if(index === player.index){
+                    cars[index - 1].shapeColor = "red";
+
+                    camera.position.x = displayWidth / 2;
+                    camera.position.y = cars[index - 1].y;
+                }
+            //displayPosition += 20;
+
+            /*textSize(15);
+            text(allPlayers[plr].name + ": " + allPlayers[plr].distance, 120, displayPosition);*/
             }
         }
 
         if(keyIsDown(UP_ARROW) && player.index !== null){
-            player.index += 50;
+            player.distance += 50;
             player.update();
         }
     }
